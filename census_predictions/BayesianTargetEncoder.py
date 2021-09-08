351b5996adfb9ec72a52b43339312d0a90d99a77
damien <dmichelle@efficity.com>
2021-09-08 20:02:38 +0200

    ✨implement kbins and target_encoding in ml pipeline

---
+++ census_predictions/BayesianTargetEncoder.py
@@ -0,0 +1,100 @@
+import functools
+
+import pandas as pd
+from sklearn.base import BaseEstimator
+from sklearn.base import TransformerMixin
+
+
+class BayesianTargetEncoder(BaseEstimator, TransformerMixin):
+    """
+    Reference: https://www.wikiwand.com/en/Bayes_estimator#/Practical_example_of_Bayes_estimators
+    Args:
+        columns (list of strs): Columns to encode.
+        weighting (int or dict): Value(s) used to weight each prior.
+        suffix (str): Suffix used for naming the newly created variables.
+    """
+
+    def __init__(self, columns=None, prior_weight=100, suffix='_mean'):
+        self.columns = columns
+        self.prior_weight = prior_weight
+        self.suffix = suffix
+        self.prior_ = None
+        self.posteriors_ = None
+
+    def fit(self, X, y=None, **fit_params):
+
+        if not isinstance(X, pd.DataFrame):
+            raise ValueError('X has to be a pandas.DataFrame')
+
+        if not isinstance(y, pd.Series):
+            raise ValueError('y has to be a pandas.Series')
+
+        X = X.copy()
+
+        # Default to using all the categorical columns
+        columns = (
+            X.select_dtypes(['object', 'category']).columns
+            if self.columns is None else
+            self.columns
+        )
+        print(columns)
+
+        names = []
+        for cols in columns:
+            if isinstance(cols, list):
+                name = '_'.join(cols)
+                names.append('_'.join(cols))
+                X[name] = functools.reduce(
+                    lambda a, b: a.astype(str) + '_' + b.astype(str),
+                    [X[col] for col in cols]
+                )
+            else:
+                names.append(cols)
+
+        # Compute prior and posterior probabilities for each feature
+        X = pd.concat((X[names], y.rename('y')), axis='columns')
+        self.prior_ = y.mean()
+        self.posteriors_ = {}
+
+        for name in names:
+            agg = X.groupby(name)['y'].agg(['count', 'mean'])
+            counts = agg['count']
+            means = agg['mean']
+            pw = self.prior_weight
+            self.posteriors_[name] = ((pw * self.prior_ + counts * means) / (pw + counts)).to_dict()
+
+        return self
+
+    def transform(self, X, y=None):
+
+        if not isinstance(X, pd.DataFrame):
+            raise ValueError('X has to be a pandas.DataFrame')
+
+        # print(self.columns)
+        print(X.dtypes)
+
+        # Default to using all the categorical columns
+        # columns = (
+        #    X.select_dtypes(['object', 'category']).columns
+        #    if self.columns is None else
+        #    self.columns
+        # )
+        # print(columns)
+        # print(self.columns)
+
+        for cols in X:
+
+            if isinstance(cols, list):
+                name = '_'.join(cols)
+                x = functools.reduce(
+                    lambda a, b: a.astype(str) + '_' + b.astype(str),
+                    [X[col] for col in cols]
+                )
+            else:
+                name = cols
+                x = X[name]
+
+            posteriors = self.posteriors_[name]
+            X[name + self.suffix] = x.map(posteriors).fillna(self.prior_).astype(float)
+
+        return X
--- census_predictions/MlPipeline.py
+++ census_predictions/MlPipeline.py
@@ -6,7 +6,9 @@ date: 03/09/2021
 import pandas as pd
 from sklearn.pipeline import Pipeline
 from sklearn.compose import ColumnTransformer
-from sklearn.preprocessing import OneHotEncoder, FunctionTransformer
+from sklearn.preprocessing import OneHotEncoder, FunctionTransformer, KBinsDiscretizer
+
+from census_predictions.BayesianTargetEncoder import BayesianTargetEncoder
 
 
 class MlPipeline:
@@ -22,7 +24,9 @@ class MlPipeline:
         categorical_features = self.get_categorical_features(df)
 
         transformers = ColumnTransformer([
-           ('cat', self.categorical_transformer_pipeline(), categorical_features)
+            ('num', self.numerical_transformer_pipeline(), numerical_features),
+            ('target_encoding', self.target_encoding_pipeline(), categorical_features),
+            ('cat', self.categorical_transformer_pipeline(), categorical_features)
         ])
         return Pipeline([
            ('transformers_pipeline', transformers),
@@ -39,6 +43,24 @@ class MlPipeline:
         return Pipeline([
             ('one_hot_encoding', OneHotEncoder())
         ])
+
+    @staticmethod
+    def numerical_transformer_pipeline():
+        """
+        Categorical transformations stored in
+        scikit pipeline
+        :return: a scikit pipeline
+        """
+        return Pipeline([
+            ('kbins', KBinsDiscretizer(n_bins=5, encode='ordinal', strategy='kmeans'))
+        ])
+
+    @staticmethod
+    def target_encoding_pipeline():
+        return Pipeline([
+            ('bayesian_target_encoding', BayesianTargetEncoder(prior_weight=10, suffix=''))
+        ])
+
 
     @staticmethod
     def get_numeric_features(df: pd.DataFrame):
